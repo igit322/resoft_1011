4bb8d16dcafa42190a473f2b272df342d8a8e316
InGyeong <103246753+igit322@users.noreply.github.com>
2022-10-13 13:02:06 +0900

    Add files via upload

--- Main/main.js
+++ Main/main.js
@@ -86,12 +86,12 @@ $(document).ready(function(){
         "mouseover":function(){
             $(this).children(".card-show").css({'transform':'translate(-70px, -100px)', 'opacity':'0', 'transition':'0.35s', 'pointer-events':'none'});
             $(this).children(".card-hide").css({'transform':'translate(0, 0)', 'opacity':'1', 'transition':'0.35s', 'pointer-events':'none'});
-            $(this).css({'transform': 'scale(1.07)', 'transition': '0.25s'});
+            $(this).css({'transform': 'scale(1.13)', 'transition': '0.5'});
         },
         "mouseout":function(){
             $(this).children(".card-show").css({'transform':'translate(0,0)', 'opacity':'1', 'transition':'0.35s', 'pointer-events':'none'});
             $(this).children(".card-hide").css({'transform':'translate(70px, 100px)', 'opacity':'0', 'transition':'0.35s', 'pointer-events':'none'});
-            $(this).css({'transform': 'scale(1)', 'transition': '0.25s'});
+            $(this).css({'transform': 'scale(1)', 'transition': '0.5s'});
         }
     });
 });  
